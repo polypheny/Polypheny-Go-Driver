e8a0cf6fcf1406bb0b72fe5fd0fb900e22e401f9
vlowingkloude <vlowingkloude@some.where>
2023-09-28 11:47:26 +0000

    moving from the private repo

---
+++ polypheny/polypheny.go
@@ -0,0 +1,37 @@
+package polypheny
+
+import (
+	"polypheny.com/protoclient"
+)
+
+type Connection struct {
+	address string
+	isOpen bool
+	client *protoclient.ProtoClient
+}
+
+func Connect(address string) *Connection {
+	client := protoclient.Connect(address)
+	conn := Connection {
+		address: address,
+		isOpen: true,
+		client: client,
+	}
+	return &conn
+}
+
+func (conn *Connection) Execute(statement string, language string) {
+	_ = conn.client.ExecuteUnprepared(statement, language)
+}
+
+func (conn *Connection) Fetch() [][]interface{} {
+	return conn.client.FetchResult()
+}
+
+func (conn *Connection) Commit() {
+	conn.client.Commit()
+}
+
+func (conn *Connection) Close() {
+	conn.client.Close()
+}
---
+++ protoclient/protoclient.go
@@ -0,0 +1,167 @@
+package protoclient
+
+import (
+	"polypheny.com/protos"
+	"github.com/google/uuid"
+        "context"
+        "google.golang.org/grpc"
+        "google.golang.org/grpc/credentials/insecure"
+        "google.golang.org/grpc/metadata"
+        "log"
+        "time"
+)
+
+const (
+        MajorApiVersion = 2
+        MinorApiVersion = 0
+)
+
+type ProtoClient struct {
+        address string
+        clientUUID string
+        connection *grpc.ClientConn
+        client protos.ProtoInterfaceClient
+        ctx context.Context
+        cancel context.CancelFunc
+        isConnected bool
+        responseStreamUnprepared protos.ProtoInterface_ExecuteUnparameterizedStatementClient
+}
+
+func newClient(address string) *ProtoClient {
+	clientUUID := uuid.New().String()
+        conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+        if err != nil {
+                log.Fatalf("did not connect: %v", err)
+        }
+        c := protos.NewProtoInterfaceClient(conn)
+        ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+        ctx = metadata.AppendToOutgoingContext(ctx, "clientUUID", clientUUID)
+        client := ProtoClient{
+                address: address,
+                clientUUID: clientUUID,
+                connection: conn,
+                client: c,
+                ctx: ctx,
+                cancel: cancel,
+                isConnected: false,
+                responseStreamUnprepared: nil,
+        }
+	return &client
+}
+
+func Connect(address string) *ProtoClient {
+	client := newClient(address)
+        request := protos.ConnectionRequest{
+                MajorApiVersion: MajorApiVersion,
+                MinorApiVersion: MinorApiVersion,
+                ClientUuid: client.clientUUID,
+        }
+	response, err := client.client.Connect(client.ctx, &request)
+        if err != nil {
+                log.Fatalf("could not connect: %v", err)
+        }
+        if response.GetIsCompatible() != true {
+                log.Fatalf("could not connect")
+        }
+	client.isConnected = true
+        return client
+}
+
+func (c *ProtoClient) Close() {
+        request := protos.DisconnectRequest{}
+        _, err := c.client.Disconnect(c.ctx, &request)
+        if err != nil {
+                log.Fatalf("could not disconnect: %v", err)
+        }
+        c.cancel()
+        c.connection.Close()
+        c.isConnected = false
+}
+
+func (c *ProtoClient) Commit() {
+	request := protos.CommitRequest{}
+	_, err := c.client.CommitTransaction(c.ctx, &request)
+	if err != nil {
+                log.Fatalf("could not commit: %v", err)
+        }
+}
+
+func (c *ProtoClient) ExecuteUnprepared(statement string, language string) bool {
+        request := protos.ExecuteUnparameterizedStatementRequest{
+                LanguageName: language,
+                Statement: statement,
+        }
+        response, err := c.client.ExecuteUnparameterizedStatement(c.ctx, &request)
+        if err != nil {
+                log.Fatalf("%v", err)
+        }
+        c.responseStreamUnprepared = response
+        return true
+}
+
+func convertValues(raw protos.ProtoValue) interface{} {
+        switch t := raw.GetType(); t {
+        case protos.ProtoValue_UNSPECIFIED:
+                return nil
+        case protos.ProtoValue_BOOLEAN:
+                return raw.GetBoolean().GetBoolean()
+        case protos.ProtoValue_INTEGER:
+                return raw.GetInteger().GetInteger()
+        case protos.ProtoValue_BIGINT:
+                return raw.GetLong().GetLong()
+        case protos.ProtoValue_DOUBLE:
+                return raw.GetDouble().GetDouble()
+        case protos.ProtoValue_FLOAT:
+                return raw.GetFloat().GetFloat()
+        case protos.ProtoValue_VARCHAR:
+                return raw.GetString_().GetString_()
+        case protos.ProtoValue_BINARY:
+                return raw.GetBinary().GetBinary()
+        case protos.ProtoValue_VARBINARY:
+                return raw.GetBinary().GetBinary()
+        case protos.ProtoValue_NULL:
+                return nil
+        case protos.ProtoValue_ROW_ID:
+                return raw.GetRowId().GetRowId()
+        default:
+                log.Fatalf("This is likely a bug: %T %v", raw, raw)
+                return nil
+        }
+        return nil
+}
+
+func (c *ProtoClient) FetchResult() [][]interface{} {
+	// the first is nil
+	result, err := c.responseStreamUnprepared.Recv()
+        if err != nil {
+                log.Fatalf("%v", err)
+        }
+	// now the second
+        result, err = c.responseStreamUnprepared.Recv()
+        if err != nil {
+                log.Fatalf("%v", err)
+        }
+        rawdata := result.GetResult()
+        frame := rawdata.GetFrame()
+	if rawdata.GetScalar() == 0 {
+                if len(frame.GetRelationalFrame().GetRows()) != 0 {
+                        rows := frame.GetRelationalFrame().GetRows()
+                        var values [][]interface{}
+                        var currentRow []interface{}
+                        for _, v := range rows {
+                                currentRow = []interface{}{}
+                                for _, z := range v.GetValues() {
+                                        currentRow = append(currentRow, convertValues(*z))
+                                }
+                                values = append(values, currentRow)
+                        }
+                        return values
+                } else if len(frame.GetDocumentFrame().GetDocuments()) != 0{
+                        return nil//frame.GetDocumentFrame()
+                } else {
+                        return nil//frame.GetGraphFrame()
+                }
+        } else {
+                return nil
+        }
+}
