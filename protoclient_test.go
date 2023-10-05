76fda96866c812fefd5d2736c1ab675790a6d035
vlowingkloude <vlowingkloude@some.where>
2023-10-05 14:36:34 +0000

    updated test cases

---
+++ protoclient_test.go
@@ -0,0 +1,12 @@
+package polypheny
+
+import (
+	"testing"
+)
+
+func TestConnect(t *testing.T) {
+	client := connect("localhost:20590")
+	if client.isConnected != true {
+		t.Fatal("Protoclient.isConnected is not set to true, but no error was caught")
+	}
+}
