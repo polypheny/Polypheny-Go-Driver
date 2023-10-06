a8628c2a1d20ef0270d05cf6a1762e56d4d381bf
vlowingkloude <vlowingkloude@some.where>
2023-10-06 07:55:20 +0000

    added some test cases

--- protoclient_test.go
+++ protoclient_test.go
@@ -1,12 +1,26 @@
 package polypheny
 
 import (
-	"testing"
+	testing "testing"
+	protos "polypheny.com/protos"
 )
 
 func TestConnect(t *testing.T) {
 	client := handleConnectRequest("localhost:20590")
 	if client.isConnected != true {
 		t.Fatal("Protoclient.isConnected is not set to true, but no error was caught")
+	}
+}
+
+func TestDuplicateConnection(t *testing.T) {
+	client := handleConnectRequest("localhost:20590")
+		request := protos.ConnectionRequest{
+                MajorApiVersion: majorApiVersion,
+                MinorApiVersion: minorApiVersion,
+                ClientUuid: client.clientUUID, // use the same uuid as the previous client
+        }
+	_, err := client.client.Connect(client.ctx, &request)
+	if err == nil {
+		t.Fatal("Expected an error when sending a connect request with a duplicated UUID")
 	}
 }
