21560f3d2a9020a3dcfed5ff811f309b306f60f2
vlowingkloude <vlowingkloude@some.where>
2023-10-10 13:44:32 +0000

    updated utils

--- utils/runtests.sh
+++ utils/runtests.sh
@@ -12,13 +12,15 @@ cd ..
 rm -rf ./build
 mkdir ./build
 
-cp -r *.go ./protos ./utils/gensh.py ./build
+cp -r ./protos ./utils/generate.go ./build/
+
+cd build && go mod init utils && go mod tidy && go run . && rm -rf protos go.mod generate.go  && cd ..
+
+cp *.go ./build
 
 cd build
 
-python3 gensh.py && chmod a+x generate.sh && ./generate.sh && cd polypheny.com/protos/ && go mod init polypheny.com/protos && go mod tidy
-
-cd ../..
+cd polypheny.com/protos/ && go mod init polypheny.com/protos && go mod tidy && cd ../..
 
 go mod init github.com/polypheny/Polypheny-Go-Driver && go mod edit -replace polypheny.com/protos=./polypheny.com/protos  && go mod tidy && clear && go test -v
 
